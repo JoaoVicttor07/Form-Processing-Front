dc29c00b7b4e0ba63d928dd9ca0a22b75c7460f8
João Victor <joaovictorsdc07@gmail.com>
2024-11-11 19:51:22 -0300

    resolvendo arquivos duplicados
    resolvido parcialmente o problema que mais de um arquivo está duplicado

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signin from './pages/Signin/index';
-<<<<<<< HEAD
-import FormList from './components/FormList';
-=======
 import Signup from './pages/Signup';
->>>>>>> feature/signupPage
 
 function App() {
-  const [forms, setForms] = useState([]);
-  const [user, setUser] = useState({ name: "Usuário Placeholder" });
-
-  const handleAddForm = (formData) => {
-    setForms([...forms, formData]);
-  };
-
-return (
-  <>
+  return (
     <Router>
       <Routes>
         <Route path="/" element={<Signin />} />
-<<<<<<< HEAD
-        <Route path="/form" element={<FormPage />} />
-=======
         <Route path="/signup" element={<Signup />} />
->>>>>>> feature/signupPage
       </Routes>
     </Router>
-
-    <div className="App">
-      <h1>Serviços Eletrônicos</h1>
-      <p>Bem-vindo, {user.name}</p>
-
-      <FormList onAddForm={handleAddForm} />
-
-      <h2>Formulários Enviados</h2>
-      <ul>
-        {forms.map((form, index) => (
-          <li key={index}>
-            <p>Problema: {form.problem}</p>
-            <p>Motivo: {form.callReason}</p>
-            <p>Produto: {form.product}</p>
-            <p>Data de Envio: {form.submitDate}</p>
-            <p>Status: {form.status}</p>
-            <p>Contato: {form.contact.email}, {form.contact.phone}</p>
-            <img src={form.photo} alt="Problema" style={{ width: '100px', height: 'auto' }} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  </>
-);
+  );
 }
 
 export default App;
