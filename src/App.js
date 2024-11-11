59bf0fa5d9947e5384215eda4d142c69a58103c6
João Victor <joaovictorsdc07@gmail.com>
2024-11-11 10:01:50 -0300

    atualização de arquivos para organização

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Login';
+import Signin from './pages/Signin/index';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Signin />} />
       </Routes>
     </Router>
   );
