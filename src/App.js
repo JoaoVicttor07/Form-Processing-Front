ef3ca5550dc3c5ae6575b3ae580ac7f6877d7f94
João Victor <joaovictorsdc07@gmail.com>
2024-11-12 15:21:21 -0300

    resolvido conflitos do merge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signin from './pages/Signin/index';
-<<<<<<< HEAD
 import Form from './pages/Form/index';
 import Signup from './pages/Signup/index';
-=======
 import Signup from './pages/Signup';
 import AdminDashboard from './pages/AdminDashboard';
->>>>>>> feature/signupPage
 
 function App() {
   const [forms, setForms] = useState([]);
@@ -23,12 +20,9 @@ return (
       <Routes>
         <Route path="/" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
-<<<<<<< HEAD
         <Route path="/form" element={<Form />} />
         <Route path="*" element={<h1>Not Found</h1>} />
-=======
         <Route path="/AdminDashboard" element={<AdminDashboard />} />
->>>>>>> feature/signupPage
       </Routes>
     </Router>
   </>
