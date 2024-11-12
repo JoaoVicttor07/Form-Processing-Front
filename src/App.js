4c1ba93299e692020bcef1299b86c7630425e753
João Victor <joaovictorsdc07@gmail.com>
2024-11-12 15:40:12 -0300

    resolvendo conflitos de arquivos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Signin from './pages/Signin/index';
-<<<<<<< HEAD
-import Signup from './pages/Signup';
-=======
 import Form from './pages/Form/index';
-import Signup from './pages/Signup/index';
+import Signin from './pages/Signin/index';
 import Signup from './pages/Signup';
 import AdminDashboard from './pages/AdminDashboard';
->>>>>>> dev
 
 function App() {
-  return (
+  const [forms, setForms] = useState([]);
+  const [user, setUser] = useState({ name: "Usuário Placeholder" });
+
+  const handleAddForm = (formData) => {
+    setForms([...forms, formData]);
+  };
+
+return (
+  <>
     <Router>
       <Routes>
         <Route path="/" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
-<<<<<<< HEAD
-      </Routes>
-    </Router>
-  );
-=======
         <Route path="/form" element={<Form />} />
         <Route path="*" element={<h1>Not Found</h1>} />
         <Route path="/AdminDashboard" element={<AdminDashboard />} />
@@ -28,7 +26,6 @@ function App() {
     </Router>
   </>
 );
->>>>>>> dev
 }
 
 export default App;
