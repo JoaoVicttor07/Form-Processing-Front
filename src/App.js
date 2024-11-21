8cdcc810e9ba12ba9eb8e89bdda4d8d2b4ea461b
João Victor <joaovictorsdc07@gmail.com>
2024-11-21 17:31:36 -0300

    corrigido path

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import Form from './pages/Form/index';
 import Signin from './pages/Signin/index';
 import Signup from './pages/Signup';
 import AdminDashboard from './pages/AdminDashboard';
-import ProtectedRoute from './components/protectedRoute';
+import ProtectedRoute from './components/ProtectedRoute'; // Certifique-se de que o nome está correto
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/signin" />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/form" element={<Form />} />
-          <Route path="/AdminDashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
-          <Route path="*" element={<h1>Not Found</h1>} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/AdminDashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
